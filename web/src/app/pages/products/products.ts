b8720cc6322d4cc92d822037cc3ed4e799d1c8f3
soaresgus <gustavosoaresbt@hotmail.com>
2026-01-13 19:02:05 -0300

    feat: implemented product filtering flow

--- web/src/app/pages/products/products.ts
+++ web/src/app/pages/products/products.ts
@@ -2,15 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products';
 import { take } from 'rxjs';
 import { IProduct } from '../../interfaces/product';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products implements OnInit {
   products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  })
 
   private readonly _productsService = inject(ProductsService);
 
@@ -18,7 +24,25 @@ export class Products implements OnInit {
     this._productsService.getProducts().pipe(take(1)).subscribe({
       next: (response) => {
         this.products = response.data;
+        this.filteredProducts = response.data;
       }
     });
+  }
+
+  filterProducts() {
+    const title = this.filterForm.value.title?.toLowerCase()
+    const status = this.filterForm.value.status?.toLowerCase()
+
+    this.filteredProducts = this.products.filter(product => {
+      const matchesTitle = title ? product.title.toLowerCase().includes(title) : true;
+      const matchesStatus = status ? product.status.toLowerCase() === status : true;
+      return matchesTitle && matchesStatus;
+    });
+  }
+
+  clearFilter() {
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('')
+    this.filteredProducts = this.products;
   }
 }
